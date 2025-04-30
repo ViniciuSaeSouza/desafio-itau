f0a64ba2460cc95da544af21a7afbc673ddd688b
Vinicius Saes <viniciusaesouza@gmail.com>
2025-04-30 17:52:27 -0300

    add @Data annotation

--- src/main/java/com/desafio/itau/api/model/Transaction.java
+++ src/main/java/com/desafio/itau/api/model/Transaction.java
@@ -1,14 +1,16 @@
 package com.desafio.itau.api.model;
 
 import jakarta.validation.constraints.*;
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
-
+@Data
 public class Transaction {
-
     @NotNull(message = "Campo 'valor' não pode ser nulo.")
-    @Min(value = 0)
+    @Min(value = 0, message = "Valor deve ser igual ou maior que 0")
     private BigDecimal valor;
 
     @NotNull(message = "Campo 'dataHora' não pode ser nulo.")
